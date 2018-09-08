1715c96bd1b6acd5690557fa2bdbb7570df4ee3c
Ivan Pashkulev <ivan.pashkulev@gmail.com>
2018-09-08 21:35:03 +0300

    Project Commit

---
+++ forex-service/src/main/java/com/vankata/forexmicroservice/ForexMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.vankata.forexmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ForexMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexMicroserviceApplication.class, args);
+    }
+}
